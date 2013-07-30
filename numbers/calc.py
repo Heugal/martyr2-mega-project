63aea3078b2279a3553d4968450e6aa4755e0c9e
Stephan A. Luchanko <tuc02096@astro.ocis.temple.edu>
2013-07-30 11:06:31 -0400

    Added some more stuff

--- numbers/calc.py
+++ numbers/calc.py
@@ -1,26 +1,25 @@
+from math import *
+
 def usage():
 	print("---BASIC CALCULATOR---\n\n")
-	print("Usage: n1 <operation> n2\n")
-	print("Supports the operations: +, -, *, /\n")
+	print("Usage: <n1> <operation> <n2>\n")
+	print("Supports the operations: +, -, *, /, !, ^, ~(square root)\n")
 	print("To exit, type exit or ^C.\n")
-
+ans = 0
 usage()
 while(1):
+	total = 0
 	calc = raw_input("Enter calculation: \n")
 	query = calc.split()
 	if calc == "exit":
 		exit(0)
-	elif len(query) != 3:
-		print("ERROR: Must input 3 arguments.\n");
-		usage()
-	else:
+	if (len(query) == 3):
 		num1 = int(query[0])
 		op = query[1]
 		num2 = int(query[2])
 		if (((isinstance(num1, int)) == False) or ((isinstance(num2, int)) == False) or ((isinstance(op, str)) == False)):
 			usage()
 		else:
-			total = 0
 			if op == "+":
 				total = num1+num2
 			elif op == "-":
@@ -33,4 +32,24 @@ while(1):
 				else:
 					print("ERROR: Cannot divide by zero.\n")
 					total = 0
-			print("%d\n" % (total))
+			elif op == "^":
+				total = pow(num1, num2)
+	elif len(query) == 2:
+		if query[1] == "ans":
+			num1 = ans
+			op = query[0]
+		else:
+			num1 = int(query[0])
+			op = query[1]
+
+		if op == "!":
+			total = factorial(num1)
+		elif op == "~":
+			if num1 < 0:
+				print("ERROR: Cannot square root a negative number.\n");
+				total = 0
+			else:
+				total = sqrt(num1)
+	
+	print("%d\n" % (total))
+	ans = total
